7848ecbab5450cd9dc3f4562e3ade544605beddf
YusukeMukoyama <yusuke.mukoyama@gmail.com>
2016-02-20 00:41:30 +0900

    debug gemfile

--- chef-repo/site-cookbooks/rbenv-ruby/attributes/default.rb
+++ chef-repo/site-cookbooks/rbenv-ruby/attributes/default.rb
@@ -1,4 +1,4 @@
 default[:rbenv][:revision]      = "master"
 default[:ruby_build][:revision] = "master"
-default[:rbenv][:build]         = "2.1.2"
+default[:rbenv][:build]         = "2.2.4"
 default[:rbenv][:bundler]       = "1.6.2"
